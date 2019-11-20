cf759dff9aadea1d7935aec2669ee049a39f82f6
YusukeKumamoto <y.kuma1229@gmail.com>
2019-11-21 00:06:54 +0900

    body-parserとルーティング追加

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,10 +2,19 @@ import Express from "express";
 
 const app = Express();
 
-app.get("/", (req, res) => {
-  const data = { hoge: "ほげ" };
-  res.send(data);
+// リクエスト解析(body-parser)
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+// ルーティング設定
+const router = Express.Router();
+router.get("/api/v1/hoge", (req, res) => {
+  res.send(req.query);
 });
+router.post("/api/v1/hoge", (req, res) => {
+  res.send(req.body);
+});
+app.use(router);
 
 const PORT = 8080;
 const HOST = "localhost";
