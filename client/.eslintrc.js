35473615bcdfe6d0155611e1edc92ccd6a657f55
YusukeKumamoto <y.kuma1229@gmail.com>
2019-12-05 11:08:22 +0900

    ルール修正

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,50 +5,67 @@ module.exports = {
     node: true
   },
   extends: [
-    "airbnb",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
-    "prettier/react"
+    'airbnb',
+    'plugin:import/typescript',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react'
   ],
-  plugins: ["react", "@typescript-eslint", "react-hooks"],
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
     warnOnUnsupportedTypeScriptVersion: false
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", "jsx", ".ts", ".tsx"]
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
       }
     }
   },
   rules: {
     // eslint
-    "no-var": "error",
-    "no-console": ["error", { allow: ["warn", "error"] }],
+    'no-var': 'error',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
     // @typescript-eslint
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-member-accessibility": "off",
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
     // react
-    "react/jsx-filename-extension": [
-      "error",
+    'react/jsx-filename-extension': [
+      'error',
       {
-        extensions: ["jsx", "tsx"]
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
       }
     ],
-    "react/jsx-one-expression-per-line": "off",
+    'react/jsx-one-expression-per-line': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-fragments': 'off',
     // react hooks
-    "react-hooks/rules-of-hooks": "error"
+    'react-hooks/rules-of-hooks': 'error',
+    // import
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    'import/prefer-default-export': 'off'
   }
-};
+}
