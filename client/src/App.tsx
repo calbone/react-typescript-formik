07c765410cec289e882afe6013fa8ce9ebd03916
YusukeKumamoto <y.kuma1229@gmail.com>
2019-12-09 23:44:06 +0900

    フォームのルーティング追加

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import FormList from './containers/FormList'
+import FormEnquete from './containers/FormEnquete'
 import './styles/app.scss'
 
 const App: React.FC = () => {
@@ -8,6 +9,8 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={FormList} />
+        <Route exact path="/form/enquete" component={FormEnquete} />
+        <Route exact path="/form/enquete/:id/edit" component={FormEnquete} />
       </Switch>
     </BrowserRouter>
   )
