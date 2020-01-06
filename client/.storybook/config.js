a8a17a696cfcc5c703440babc5801ebc4cf025e1
YusukeKumamoto <y.kuma1229@gmail.com>
2020-01-06 23:58:30 +0900

    story設定

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,4 +1,17 @@
-import { configure } from '@storybook/react';
+import { configure } from '@storybook/react'
+import { setConsoleOptions } from '@storybook/addon-console'
+
+setConsoleOptions({
+  panelExclude: []
+})
+
+const loadStories = () => {
+  let req = require.context('../src/stories', true, /.(tsx|js)$/)
+  req.keys().forEach(filename => req(filename))
+
+  req = require.context('../src', true, /.stories.(tsx|js)$/)
+  req.keys().forEach(filename => req(filename))
+}
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+configure(loadStories, module)
