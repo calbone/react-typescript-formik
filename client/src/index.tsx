5d55ff5a39010eea40b2d808ca7680d34687989b
YusukeKumamoto <y.kuma1229@gmail.com>
2020-01-16 00:56:20 +0900

    redux-devtools-extension追加とEnhancerの修正

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { ThemeProvider } from 'styled-components'
 import rootReducer from './stores/domain/form/reducers'
@@ -9,16 +9,25 @@ import rootSaga from './stores/domain/form/sagas'
 import { themes } from './constants/themes'
 import GlobalStyle from './components/GlobalStyle'
 import App from './App'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 const sagaMiddleware = createSagaMiddleware()
-interface ExtendedWindow extends Window {
-  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
-}
-declare const window: ExtendedWindow
 
-const composeReduxDevToolsEnhancers =
-  (typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose
+const composeReduxDevToolsEnhancers = composeWithDevTools({
+  features: {
+    pause: true, // start/pause recording of dispatched actions
+    lock: true, // lock/unlock dispatching actions and side effects
+    persist: true, // persist states on page reloading
+    export: true, // export history of actions in a file
+    import: 'custom', // import history of actions from a file
+    jump: false, // jump back and forth (time travelling)
+    skip: true, // skip (cancel) actions
+    reorder: true, // drag and drop actions in the history list
+    dispatch: true, // dispatch custom actions or action creators
+    test: true // generate tests for the selected actions
+  }
+  // other options like actionSanitizer, stateSanitizer
+})
 
 const store = createStore(
   rootReducer(),
