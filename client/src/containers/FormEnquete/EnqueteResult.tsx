26fc9586da7d2360bcb6914e784e5c29c6cf771a
YusukeKumamoto <y.kuma1229@gmail.com>
2020-02-08 23:40:48 +0900

    styledComponets化

--- client/src/containers/FormEnquete/EnqueteResult.tsx
+++ client/src/containers/FormEnquete/EnqueteResult.tsx
@@ -25,50 +25,48 @@ const EnqueteResult: React.FC<AnswerSummaryProps> = ({ answerSummary }) => {
       font-size: 20px;
     }
   `
-  // const PopMenu = styled.div`
-  //   display: flex;
-  //   flex: 0 0 auto;
-  //   align-items: center;
-  //   position: relative;
-  //   cursor: pointer;
-  //   svg {
-  //     width: 24px;
-  //     height: 24px;
-  //     fill: ${({ theme }) => theme.colors.mainC};
-  //     min-width: 16px;
-  //   }
-  //   &:hover {
-  //     .popMenu__select {
-  //       visibility: visible;
-  //       opacity: 1;
-  //       z-index: 1;
-  //     }
-  //   }
-  //   .popMenu__select {
-  //     visibility: hidden;
-  //     opacity: 0;
-  //     position: absolute;
-  //     top: calc(100% + 5px);
-  //     right: 0;
-  //     z-index: -1;
-  //     border-radius: 2px;
-  //     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  //     background-color: ${({ theme }) => theme.colors.baseC};
-  //     white-space: nowrap;
-  //     cursor: pointer;
-  //     transition: 0.3s;
-  //   }
-  //   li + li {
-  //     border-top: 1px solid ${({ theme }) => theme.colors.baseA};
-  //   }
-  //   button {
-  //     width: 100%;
-  //     padding: 10px 65px;
-  //     font-weight: bold;
-  //     font-size: 14px;
-  //     line-height: 1.5;
-  //   }
-  // `
+  const ResultGuide = styled.div`
+    padding: 0 16px;
+    line-height: 2.2;
+  `
+
+  const Box = styled.div`
+    border-radius: 2px;
+    background-color: #fff;
+    &:not(:first-child) {
+      margin-top: 16px;
+    }
+  `
+
+  const Part = styled.div`
+    padding: 16px;
+    &:not(:last-child) {
+      border-bottom: ${({ theme }) => theme.colors.baseA} solid 1px;
+    }
+    .resultTitle {
+      margin-bottom: 12px;
+      font-weight: bold;
+    }
+    .answerNum {
+      margin-bottom: 20px;
+      color: ${({ theme }) => theme.colors.mainC};
+      font-size: 12px;
+    }
+    .resultItem {
+      padding-left: 1em;
+      text-indent: -1em;
+      &:not(:last-child) {
+        margin-bottom: 16px;
+      }
+      &--vote {
+        display: flex;
+        justify-content: space-between;
+        span {
+          font-weight: bold;
+        }
+      }
+    }
+  `
   const menuList = ['Shift-JIS', 'UTF-8']
   return (
     <React.Fragment>
@@ -82,53 +80,43 @@ const EnqueteResult: React.FC<AnswerSummaryProps> = ({ answerSummary }) => {
         <PopMenu label={'CSVダウンロード'} menuList={menuList}>
           <Icon type="download" />
         </PopMenu>
-        {/*CSVダウンロード
-          <ul className="popMenu__select">
-            <li>
-              <button>Shift-JIS</button>
-            </li>
-            <li>
-              <button>UTF-8</button>
-            </li>
-          </ul> */}
       </ResultSummary>
-      <div className="p-formEnquete__resultGuide">
+      <ResultGuide>
         個人情報（メールアドレス、住所）はCSVをダウンロードしてご確認いただけます。
         <br />
         CSVはPCサイトのみで確認できます。
-      </div>
-      <div className="c-box">
+      </ResultGuide>
+      <Box>
         {answer_detail_summaries.map((answer, i) => {
+          console.log(
+            'answer.answer_detail_summary.summary:',
+            answer.answer_detail_summary.summary
+          )
           return (
-            <div className="p-formEnquete__part" key={i}>
-              <div className="c-box__body">
-                <p className="p-formEnquete__resultTitle">
-                  {answer.answer_detail_summary.question_title}
-                </p>
-                <div className="p-formEnquete__answerNum">
-                  回答 {answer.answer_detail_summary.answer_count}件
-                </div>
-                {/* {answer.answer_detail_summary.summary.length > 0 &&
-                  answer.answer_detail_summary.summary.map((summary, j) => {
-                    return isObject(summary) ? (
-                      <div
-                        className="p-formEnquete__resultItem p-formEnquete__resultItem--vote"
-                        key={j}
-                      >
-                        {j + 1}. {summary.choice_title}
-                        <span>{summary.choice_count}票</span>
-                      </div>
-                    ) : (
-                      <div className="p-formEnquete__resultItem" key={j}>
-                        {j + 1}. {summary}
-                      </div>
-                    )
-                  })} */}
+            <Part key={i}>
+              <p className="resultTitle">
+                {answer.answer_detail_summary.question_title}
+              </p>
+              <div className="answerNum">
+                回答 {answer.answer_detail_summary.answer_count}件
               </div>
-            </div>
+              {answer.answer_detail_summary.summary.length > 0 &&
+                answer.answer_detail_summary.summary.map((summary, j) => {
+                  return isObject(summary) ? (
+                    <div className="resultItem resultItem--vote" key={j}>
+                      {j + 1}. {summary.choice_title}
+                      <span>{summary.choice_count}票</span>
+                    </div>
+                  ) : (
+                    <div className="resultItem" key={j}>
+                      {j + 1}. {summary}
+                    </div>
+                  )
+                })}
+            </Part>
           )
         })}
-      </div>
+      </Box>
     </React.Fragment>
   )
 }
