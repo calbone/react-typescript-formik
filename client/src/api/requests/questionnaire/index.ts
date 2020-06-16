8543cb5b81dbfa3d1aad70015065fcc2c904843c
YusukeKumamoto <y.kuma1229@gmail.com>
2020-06-17 00:02:25 +0900

    QuestionnaireResponse型追加

--- client/src/api/requests/questionnaire/index.ts
+++ client/src/api/requests/questionnaire/index.ts
@@ -1,24 +1,29 @@
 import request from '../..'
 import { Questionnaire } from 'types/questionnaire'
+
+interface QuestionnairesResponse {
+  data: Questionnaire[]
+}
+
+interface QuestionnaireResponse {
+  data: Questionnaire
+}
+
 // export const createQuestionnairesRequest = async payload => {
 //   const url = "/api/v2/current_user/owner/questionnaires";
 //   const data = await request.post(url, payload);
 //   return data;
 // };
 
-interface QuestionnaireResponse {
-  data: Questionnaire[]
-}
-
 export const readQuestionnairesRequest = async () => {
   const url = '/api/v2/current_user/owner/questionnaires'
-  const { data } = await request.get<QuestionnaireResponse>(url)
+  const { data } = await request.get<QuestionnairesResponse>(url)
   return data
 }
 
 export const readQuestionnaireRequest = async (payload: { id: string }) => {
   const url = `/api/v2/current_user/owner/questionnaires/${payload.id}`
-  const data = await request.get(url)
+  const data = await request.get<QuestionnaireResponse>(url)
   return data
 }
 
