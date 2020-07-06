c366043bc1c21f3383345facae0fbffc923dcbdf
YusukeKumamoto <y.kuma1229@gmail.com>
2020-07-06 23:52:35 +0900

    useField追加

--- client/src/components/TextField/AddTextField.tsx
+++ client/src/components/TextField/AddTextField.tsx
@@ -1,20 +1,26 @@
 import * as React from 'react'
 import styled from 'styled-components'
+import { useField } from 'formik'
 import TextFieldStyle from './style'
 
 type AddTextFieldProps = {
-  name?: string
+  name: string
   placeholder?: string
 }
 
-const AddTextField: React.FC<AddTextFieldProps> = ({ name, placeholder }) => {
-  return <StyledTextField name={name} placeholder={placeholder} />
+const AddTextField: React.FC<AddTextFieldProps> = ({
+  name,
+  placeholder,
+  ...restProps
+}) => {
+  const [field] = useField(name)
+  return <StyledTextField {...field} {...restProps} placeholder={placeholder} />
 }
 
 const StyledTextField = styled.input.attrs(({ name, placeholder }) => ({
   type: 'type',
   name,
-  placeholder
+  placeholder,
 }))`
   ${TextFieldStyle}
   width: 95%;
