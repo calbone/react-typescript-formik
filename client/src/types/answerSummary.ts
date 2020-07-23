693fa519d6b5160611a874774d35f5be453a4329
YusukeKumamoto <y.kuma1229@gmail.com>
2020-07-23 23:28:03 +0900

    type追加

--- client/src/types/answerSummary.ts
+++ client/src/types/answerSummary.ts
@@ -1,18 +1,20 @@
-interface AnswerDetailSummaries {
-  answer_detail_summary: AnswerDetailSummary
+type InputType = 'nickname' | 'choice' | 'selectbox' | 'text'
+
+interface Summary {
+  choice_title: string
+  choice_count: number
 }
 
 interface AnswerDetailSummary {
   question_uuid: string
   question_title: string
-  type: 'nickname' | 'choice' | 'selectbox' | 'text'
+  type: InputType
   answer_count: number
   summary: Summary[]
 }
 
-type Summary = {
-  choice_title: string
-  choice_count: number
+interface AnswerDetailSummaries {
+  answer_detail_summary: AnswerDetailSummary
 }
 
 export interface AnswerSummary {
