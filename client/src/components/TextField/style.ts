6ddbf57260ee5dcebb1130a8dcbefcd65be5feba
YusukeKumamoto <y.kuma1229@gmail.com>
2020-07-30 00:26:57 +0900

    style修正

--- client/src/components/TextField/style.ts
+++ client/src/components/TextField/style.ts
@@ -2,13 +2,15 @@ import { css } from 'styled-components'
 
 const TextFieldStyle = css`
   position: relative;
-  width: 100%;
   padding: 8px 14px;
   border: 1px solid ${({ theme }) => theme.colors.mainD};
   border-radius: 4px;
   background-color: ${({ theme }) => theme.colors.baseC};
   line-height: 1.714;
   resize: none;
+  input {
+    width: 100%;
+  }
 `
 
 export default TextFieldStyle
