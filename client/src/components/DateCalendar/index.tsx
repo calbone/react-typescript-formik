2e2e34692383875081b6d0bcdacea4a4abbb27e0
YusukeKumamoto <y.kuma1229@gmail.com>
2020-08-12 23:57:42 +0900

    date修正

--- client/src/components/DateCalendar/index.tsx
+++ client/src/components/DateCalendar/index.tsx
@@ -16,41 +16,42 @@ const DateCalendarComponent: React.FC<DateCalendarProps> = ({
   name,
   className,
 }) => {
-  const [startDate, setStartDate] = useState<Date | undefined>()
-  const [endDate, setEndDate] = useState<Date | undefined>()
+  const [startDate, setStartDate] = useState<Date | undefined>(new Date())
+  const [endDate, setEndDate] = useState<Date | undefined>(new Date())
   const { setFieldValue } = useFormikContext()
   const [field] = useField(name)
   const { value, onChange, ...restField } = field
+
   const handleStartDateChange = useCallback(
     (date: Date) => {
       setStartDate(date)
       setFieldValue(name, date)
     },
-    [name, setFieldValue, endDate, startDate]
+    [name, setFieldValue]
   )
-  const handleEndDateChange = useCallback(
-    (date: Date) => {
-      setEndDate(date)
-      if (startDate && dayjs(endDate).isAfter(dayjs(startDate))) {
-        setFieldValue(name, date)
-      }
+  console.log('startDate', startDate)
 
-      console.log('endDate', endDate)
-      setFieldValue(name, date)
-      // if(dayjs(endDate).isAfter(dayjs(startDate))) {
-      //   setFieldValue(name, date);
-      // } else {
-      //   console.log("開始日時より後を選択してください。");
-      // }
-    },
-    [name, setFieldValue, endDate, startDate]
-  )
+  // const handleEndDateChange = useCallback(
+  //   (date: Date) => {
+  //     console.log('handleEndDateChange入った!')
+  //     setEndDate(date)
+  //     console.log('endDate', endDate)
+  //     if (dayjs(endDate).isBefore(dayjs(startDate))) {
+  //       console.log('dayのなか')
+  //       alert('開始日時より後を選択してください。')
+  //     } else {
+  //       setFieldValue(name, date)
+  //     }
+  //   },
+  //   [name, setFieldValue, startDate, endDate]
+  // )
+  console.log('endDate', endDate)
   return (
     <div className={className}>
       <div className="datePicker">
         <DatePicker
           selected={value}
-          onChange={name === '' ? handleStartDateChange : handleStartDateChange}
+          onChange={handleStartDateChange}
           dateFormat="yyyy/MM/dd HH:mm"
           timeFormat="HH:mm"
           showTimeSelect
