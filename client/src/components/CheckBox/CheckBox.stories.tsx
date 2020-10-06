f35890553a44191f5827529c27787ae0478d9df0
YusukeKumamoto <y.kuma1229@gmail.com>
2020-10-06 18:17:54 +0900

    storybook-formik追加

--- client/src/components/CheckBox/CheckBox.stories.tsx
+++ client/src/components/CheckBox/CheckBox.stories.tsx
@@ -1,13 +1,17 @@
-import * as React from 'react'
+import React from 'react'
 import { storiesOf } from '@storybook/react'
+import withFormik from 'storybook-formik'
 import { text } from '@storybook/addon-knobs'
 import CheckBox from './'
+import { defaultValues } from '../../constants/defaultValues'
 
 const stories = storiesOf('CheckBox', module)
-stories.add('Base', () => (
-  <CheckBox
-    name={text('フォーム名', 'require')}
-    value={text('id', '1')}
-    label={text('テキスト', '必須')}
-  />
-))
+stories
+  .addDecorator(withFormik)
+  .add('default', () => (
+    <CheckBox
+      name={text('フォーム名', 'question_data.questions[0].required')}
+      value={text('id', '1')}
+      label={text('テキスト', '必須')}
+    />
+  ))
