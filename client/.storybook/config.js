5e6ff7f090b998dcf2e06cd4f4459a91bacd1ac2
YusukeKumamoto <y.kuma1229@gmail.com>
2020-10-08 17:59:35 +0900

    @storybook/addon-a11y追加

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -2,13 +2,14 @@ import * as React from 'react'
 import { configure, addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import { withKnobs } from '@storybook/addon-knobs'
+import { withA11y } from '@storybook/addon-a11y'
 import { setConsoleOptions } from '@storybook/addon-console'
 import { themes } from '../src/constants/themes'
 import { ThemeProvider } from 'styled-components'
 import requireContext from 'require-context.macro'
 
 setConsoleOptions({
-  panelExclude: []
+  panelExclude: [],
 })
 
 const req = requireContext(
@@ -19,8 +20,11 @@ const req = requireContext(
 const loadStories = () => {
   addDecorator(withKnobs)
   addDecorator(withInfo)
-  addDecorator(story => <ThemeProvider theme={themes}>{story()}</ThemeProvider>)
-  req.keys().forEach(filename => req(filename))
+  addDecorator(withA11y)
+  addDecorator((story) => (
+    <ThemeProvider theme={themes}>{story()}</ThemeProvider>
+  ))
+  req.keys().forEach((filename) => req(filename))
 }
 
 // automatically import all files ending in *.stories.js
